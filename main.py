dc736cb1e8cd8b1a1e72da0270470d7949499378
Wei ZHANG <yidazhang1@gmail.com>
2023-08-20 19:43:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -115,8 +115,8 @@ if __name__ == "__main__":
         # 转换当前时间为指定格式
         new_time_str = convert_time(current_shanghai_time) + f" `{current_shanghai_time.strftime('%Y-%m-%d %H:%M:%S')}`"
         print(new_time_str) #:clock7 `2023-08-20 18:59:44`
-        pattern = r'最近更新时间 (.*?) `(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})`'
-        updated_content = re.sub(pattern, '最近更新时间 ' + new_time_str, new_readme)
+        pattern = r"最近更新时间 (.*?) `(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})`"
+        updated_content = re.sub(pattern, "最近更新时间 " + new_time_str, new_readme)
         repo.update_file(path=contents.path, message=COMMIT_MESSAGE,
                          content=updated_content, sha=contents.sha)
         
