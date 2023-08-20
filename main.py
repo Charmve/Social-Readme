a257f0a4e7e29a8cc6c5dde15aa207a6e7769215
Wei ZHANG <yidazhang1@gmail.com>
2023-08-20 18:02:44 +0800

    Update clock in readme

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import base64
 import os
 import sys
 from github import Github, GithubException
+import re
+from datetime import datetime
+import pytz
 
 import social
 
@@ -23,6 +26,51 @@ def decode_readme(data: str) -> str:
     """Decode the contents of old readme"""
     decoded_bytes = base64.b64decode(data)
     return str(decoded_bytes, 'utf-8')
+
+# 获取当前上海时区时间
+def get_shanghai_time():
+    shanghai_tz = pytz.timezone('Asia/Shanghai')
+    return datetime.now(shanghai_tz)
+
+# 根据规则转换时间
+def convert_time(current_time):
+    hour = current_time.hour
+    if hour == 0:
+        hour12 = 12
+        am_pm = 'AM'
+    elif hour < 12:
+        hour12 = hour
+        am_pm = 'AM'
+    elif hour == 12:
+        hour12 = 12
+        am_pm = 'PM'
+    else:
+        hour12 = hour - 12
+        am_pm = 'PM'
+
+    minute = current_time.minute
+    if minute >= 0 and minute <= 15:
+        time_str = f":clock{hour12}"
+    elif minute > 15 and minute <= 30:
+        time_str = f":clock{hour12}30"
+    elif minute > 30 and minute <= 45:
+        time_str = f":clock{hour12}30"
+    elif minute > 45 and minute <= 60:
+        time_str = f":clock{hour12 + 1}"
+    else:
+        time_str = f":clock{hour12}30"
+
+    return time_str
+
+# 读取并更新文件内容
+def update_file_content(file_path, new_time_str):
+    with open(file_path, 'r') as file:
+        content = file.read()
+
+    updated_content = re.sub(r':clock\d{1,4} `(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})`', new_time_str, content)
+
+    with open(file_path, 'w') as file:
+        file.write(updated_content)
 
 
 if __name__ == "__main__":
@@ -60,4 +108,14 @@ if __name__ == "__main__":
         print("readme change, start update ...")
         repo.update_file(path=contents.path, message=COMMIT_MESSAGE,
                          content=new_readme, sha=contents.sha)
-        print("your readme update completed!")
+        
+        # 获取当前上海时区时间
+        current_shanghai_time = get_shanghai_time()
+        # 转换当前时间为指定格式
+        new_time_str = convert_time(current_shanghai_time) + f" `{current_shanghai_time.strftime('%Y-%m-%d %H:%M:%S')}`"
+        print(new_time_str)
+        updated_content = re.sub(r':clock\d{1,4} `(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})`', new_time_str, new_readme)
+        repo.update_file(path=contents.path, message=COMMIT_MESSAGE,
+                         content=updated_content, sha=contents.sha)
+        
+        print(" ✅ Your readme update completed!")
